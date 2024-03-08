267aa7613d9a3ba72f8c35381aaefce6806fa5dc
NicoloCasagrande <nicolo.casagrande7@gmail.com>
2024-03-08 16:22:14 +0100

    fix

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/main.js', // Update the entry file path accordingly
+  entry: './src/main.jsx', // Update the entry file path accordingly
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'), // Update the output directory accordingly
