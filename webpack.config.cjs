0ae5d238f208a852968c081bd0f961c520a271de
NicoloCasagrande <nicolo.casagrande7@gmail.com>
2024-03-08 16:52:21 +0100

    fix

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,9 +1,27 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/main.jsx', // Update the entry file path accordingly
+  entry: './src/main.jsx',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'), // Update the output directory accordingly
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
