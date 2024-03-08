c422641c91cf7a21bae023240ddcb192efcf1f9f
NicoloCasagrande <nicolo.casagrande7@gmail.com>
2024-03-08 16:00:38 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    target: 'esnext',
-  },
   base: "/chat-reactjs/",
   plugins: [react()],
 });
