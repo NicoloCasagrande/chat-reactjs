6ffb33ae0a1d7f01b6a141dbbdc8b67b0a85f269
NicoloCasagrande <nicolo.casagrande7@gmail.com>
2024-03-09 01:19:58 +0100

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/chat-reactjs/",
+  build: {
+    outDir: 'dist', // Specify the output directory for the production build
+    assetsInlineLimit: 512 * 1024, // Adjust the inline limit for assets (optional)
+  },
   plugins: [react()],
 });
